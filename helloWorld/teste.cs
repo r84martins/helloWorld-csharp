04d7fc5f76aa5bab3050eb21f12bfa9e9402dc64
Ricardo Martins <ricardo.3.martins@continental-corporation.com>
2021-06-25 13:10:25 -0300

    Projeto Iniciado - ComObj hello world

---
+++ helloWorld/teste.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace helloWorld
+{
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("hello.world")]
+    public class teste
+    {
+
+        public teste() { }
+        public string test()
+        {
+            return "Bruno Bixinha";
+        }
+        
+    }
+}
